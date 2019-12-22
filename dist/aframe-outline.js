f8e136fa75b1a196a133019bda61288cdea2c71e
SirFizX <eeisaman@auburnschl.edu>
2019-12-22 17:23:27 -0500

    Update aframe-outline.js

--- dist/aframe-outline.js
+++ dist/aframe-outline.js
@@ -1,14 +1,20 @@
 AFRAME.registerComponent('outline', {
   schema: {
-    color: {default: 'red'}
+    color: {default: 'red'},
+    scale: {default: 1.05},
+    pulse: {default: false},
+    frequency: {default: 1}
   },
   
   init: function(){ 
     
     this.material = new THREE.MeshBasicMaterial( { color: this.data.color , side: THREE.BackSide } );
     this.mesh = new THREE.Mesh( this.el.components.geometry.geometry, this.material );
-    this.mesh.scale.multiplyScalar(1.05);
+    this.scale = this.data.scale;
+    this.mesh.scale.multiplyScalar(this.scale);
     this.el.object3D.add( this.mesh );
+    this.frequency = this.data.frequency;
+    
     
   },
   
@@ -18,6 +24,28 @@ AFRAME.registerComponent('outline', {
     this.material.needsUpdate = true;
     
   },
+  
+  tick: function(t,dt){
+    
+     if(this.data.pulse) 
+        this.mesh.scale.setScalar( (0.04*Math.cos(this.frequency*t/120) ) + this.scale);
+    
+  
+  },
+  
+  pulse: function(frequency){
+  
+    this.frequency = frequency ? frequency:1;
+    this.data.pulse = true;
+  
+  },
+  
+  stopPulse: function(){
+  
+    this.data.pulse = false;
+    this.scale = this.data.scale;
+  
+  }
   
   
 });
